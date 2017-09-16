d9d20880302462a0cc6d321954ab75739e4d5c9b
Leandro Lisboa Penz <lpenz@lpenz.org>
2017-09-16 21:16:12 +0100

    tests: create tmp file with "with" to avoid leaks

--- tests/omnilint_test_common.py
+++ tests/omnilint_test_common.py
@@ -9,11 +9,11 @@ import omnilint.reporters as reporters
 def checkthis(checkername, fileext, contents):
     ol = omnilint.Omnilint()
     ol.checker_load(checkername)
-    tmp = tempfile.NamedTemporaryFile(suffix=fileext)
-    tmp.write(contents.encode('utf-8'))
-    tmp.flush()
-    with reporters.ReporterList() as reporter:
-        ol.analyse_file(reporter, tmp.name)
-        for e in reporter.list:
-            e.pop('file')
-        return reporter.list
+    with tempfile.NamedTemporaryFile(suffix=fileext) as tmp:
+        tmp.write(contents.encode('utf-8'))
+        tmp.flush()
+        with reporters.ReporterList() as reporter:
+            ol.analyse_file(reporter, tmp.name)
+            for e in reporter.list:
+                e.pop('file')
+            return reporter.list
